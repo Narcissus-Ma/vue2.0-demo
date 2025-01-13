899e6b8c0eb86eb00f4125d73833e8357da9cc26
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2025-01-13 13:29:10 +0800

    feat: 添加部署gh-pages指令

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
   lintOnSave: false,
   pluginOptions: {
     useYarn: false, // 强制禁用yarn
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue2.0-demo/' : '/' // 部署时使用
 });
